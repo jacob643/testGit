8a151279d11e7d9fc35db7597dbd7b025231bb9a
Billy <billy.bouchard03@gmail.com>
2018-09-07 15:00:34 -0400

    add routes to initial-view

--- client/src/app/router/approuter.module.ts
+++ client/src/app/router/approuter.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { InitalViewComponent } from '../inital-view/inital-view.component'
+
 export const routes: Routes = [
-]
+    { path: '', redirectTo: 'initial', pathMatch: 'full' },
+    { path: 'initial', component: InitalViewComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
