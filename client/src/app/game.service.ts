ad8200d04cc4b54b3b1d0a836d5285a921598743
FX <francois-xavier.legault@polymtl.ca>
2018-09-07 16:53:28 -0400

    Corrected syntax errors.

--- client/src/app/game.service.ts
+++ client/src/app/game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Game } from './../../../common/game/game.ts';
+import { Game } from './../../../common/game/game';
 
 @Injectable({
     providedIn: 'root'
@@ -9,23 +9,24 @@ export class GameService {
     constructor() { }
 
     generateExample():Game {
-        Game gameExample = {
-            id = 0,
-            name = "gameTest",
-            soloScore = {
-                id= 0,
-                first = { id= 0, name= "testUser0", time = "01:00" },
-                second = { id= 1, name= "testUser1", time = "02:00" },
-                third = { id= 2, name= "testUser2", time = "03:00" }
-            },
-            1v1Score = {
-                id= 1,
-                first = { id= 3, name= "testUser3", time = "01:00" },
-                second = { id= 4, name= "testUser4", time = "02:00" },
-                third = { id= 5, name= "testUser5", time = "03:00" }
-            },
-            imagePath = "../assets/gameImage/gameTest",
-            singleView = true
+        let gameExample: Game;
+        gameExample = {
+          id: 0,
+          name: "gameTest",
+          scoreSolo: {
+              id: 0,
+              first: { id: 0, name: "testUser0", time : "01:00" },
+              second: { id: 1, name: "testUser1", time : "02:00" },
+              third: { id: 2, name: "testUser2", time : "03:00" }
+          },
+          score1v1: {
+              id: 1,
+              first : { id: 3, name: "testUser3", time : "01:00" },
+              second : { id: 4, name: "testUser4", time : "02:00" },
+              third : { id: 5, name: "testUser5", time : "03:00" }
+          },
+          imagePath: "../assets/gameImage/gameTest",
+          singleView: true
         }
 
         return gameExample;
