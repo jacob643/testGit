8eb09880c3940f00e55f4e4db6064c2b4d96cdff
Billy <billy.bouchard03@gmail.com>
2018-09-07 14:59:44 -0400

    fix merge conflict

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,8 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
 import { HttpClientModule } from "@angular/common/http";
-<<<<<<< HEAD
 import { InitalViewComponent } from './inital-view/inital-view.component';
-=======
 import { AppRouterModule } from "./router/approuter.module";
->>>>>>> 540495f8f8a14d31603ac9830a9a738888c2e2ab
 
 @NgModule({
     declarations: [
