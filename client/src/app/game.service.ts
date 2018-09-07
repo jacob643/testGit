376cd9822aceae7d9cd8e7029233ef038db619ae
FX <francois-xavier.legault@polymtl.ca>
2018-09-07 16:17:24 -0400

    New method generateExample that generates a test game object.

--- client/src/app/game.service.ts
+++ client/src/app/game.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Game } from './../../../common/game/game.ts';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+    constructor() { }
+
+    generateExample() {
+        Game gameExample = {
+            id = 0,
+            name = "gameTest",
+            soloScore = {
+                id= 0,
+                first = { id= 0, name= "testUser0", time = "01:00" },
+                second = { id= 1, name= "testUser1", time = "02:00" },
+                third = { id= 2, name= "testUser2", time = "03:00" }
+            },
+            1v1Score = {
+                id= 1,
+                first = { id= 3, name= "testUser3", time = "01:00" },
+                second = { id= 4, name= "testUser4", time = "02:00" },
+                third = { id= 5, name= "testUser5", time = "03:00" }
+            },
+            imagePath = "../assets/gameImage/gameTest",
+            singleView = true
+        }
+    }
 }
