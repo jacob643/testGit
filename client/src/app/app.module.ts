b6cbee0100a6b9518549f2cac3dc3703c08e0d85
Audrey Germain <audrey.germain@polymtl.ca>
2018-09-07 13:42:17 -0400

    generation of component for admin view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BasicService } from "./basic.service";
 import { HttpClientModule } from "@angular/common/http";
+import { AdminViewComponent } from './admin-view/admin-view.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AdminViewComponent
     ],
     imports: [
         BrowserModule,
