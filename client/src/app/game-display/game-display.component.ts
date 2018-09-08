c55a952d655545f7fa4d668f8ab827f15eaf1e4b
FX <francois-xavier.legault@polymtl.ca>
2018-09-08 05:45:45 -0400

    Injected gameService into game-display component.

--- client/src/app/game-display/game-display.component.ts
+++ client/src/app/game-display/game-display.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from './../../../../common/game/game';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-game-display',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameDisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
   }
