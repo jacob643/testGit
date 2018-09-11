8d0a9edda37bea19d0b6d748f78ee0bb61a47337
Billy <billy.bouchard03@gmail.com>
2018-09-11 11:23:08 -0400

    Add userController

---
+++ server/app/routes/userController.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { User } from "../../../common/user/user";
+import "reflect-metadata";
+import { injectable, } from "inversify";
+
+export module Route {
+
+    @injectable()
+    export class UserController {
+        users: User[];
+
+        public get(req: Request, res: Response, next: NextFunction): void {
+            res.send(JSON.stringify(this.users));
+        }
+    }
+}
