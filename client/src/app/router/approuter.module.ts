e27fabb7cb0cabf2b0f4ce9287f113c738043c89
Audrey Germain <audrey.germain@polymtl.ca>
2018-09-11 16:11:15 -0400

    change the route gamelist to link with game menu component

--- client/src/app/router/approuter.module.ts
+++ client/src/app/router/approuter.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { InitialViewComponent } from '../inital-view/initial-view.component'
-import { GameDisplayComponent } from '../game-display/game-display.component'
+import { GameMenuComponent } from '../game-menu/game-menu.component'
 
 export const routes: Routes = [
     { path: '', redirectTo: 'initial', pathMatch: 'full' },
     { path: 'initial', component: InitialViewComponent },
-    { path: 'gamelist', component: GameDisplayComponent}
+    { path: 'gamelist', component: GameMenuComponent}
 ];
 
 @NgModule({
