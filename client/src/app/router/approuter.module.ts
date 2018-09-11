d9d16a98e323672534a4c6eb98018627364cceed
FX <francois-xavier.legault@polymtl.ca>
2018-09-11 15:16:50 -0400

    Adding 'gamelist' to url now show the game-menu page.

--- client/src/app/router/approuter.module.ts
+++ client/src/app/router/approuter.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { InitialViewComponent } from '../inital-view/initial-view.component'
+import { GameDisplayComponent } from '../game-display/game-display.component'
 
 export const routes: Routes = [
     { path: '', redirectTo: 'initial', pathMatch: 'full' },
-    { path: 'initial', component: InitialViewComponent }
+    { path: 'initial', component: InitialViewComponent },
+    { path: 'gamelist', component: GameDisplayComponent}
 ];
 
 @NgModule({
