3e50f975db0c116fa725da93e973b924edd2c5a2
Billy <billy.bouchard03@gmail.com>
2018-09-11 11:32:16 -0400

    Add get users to routes

--- server/app/routes.ts
+++ server/app/routes.ts
@@ -3,17 +3,21 @@ import { Router, Request, Response, NextFunction } from "express";
 
 import Types from "./types";
 import { Route } from "./routes/index";
+import { UserController } from "./routes/userController";
 
 @injectable()
 export class Routes {
 
-    public constructor(@inject(Types.Index) private index: Route.Index) { }
+    public constructor(@inject(Types.Index) private index: Route.Index, @inject(Types.Users) private users: UserController.Users) { }
 
     public get routes(): Router {
         const router: Router = Router();
 
         router.get("/",
             (req: Request, res: Response, next: NextFunction) => this.index.helloWorld(req, res, next));
+
+        router.get("/users",
+            (req: Request, res: Response, next: NextFunction) => this.users.get(req, res, next));
 
         return router;
     }
