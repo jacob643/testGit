40dadc16fe9c49922ee1cf3a26608637b98a88fe
jacob643 <jacobdorais25@hotmail.com>
2018-09-12 13:44:13 -0400

    indentation

--- client/src/app/stopwatch/stopwatch.component.ts
+++ client/src/app/stopwatch/stopwatch.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-stopwatch',
-  templateUrl: './stopwatch.component.html',
-  styleUrls: ['./stopwatch.component.scss']
+    selector: 'app-stopwatch',
+    templateUrl: './stopwatch.component.html',
+    styleUrls: ['./stopwatch.component.scss']
 })
 export class StopwatchComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
