1a8be8c8845c37e3e5327ebcc4f6662730cc2e45
Billy <billy.bouchard03@gmail.com>
2018-09-12 12:22:06 -0400

    Add test for getUsers method to UserController

--- server/app/routes/userController.spec.ts
+++ server/app/routes/userController.spec.ts
@@ -1,13 +1,29 @@
-import { assert } from "chai"
+import { NextFunction } from "express";
+import { assert, expect } from "chai";
+import { mockReq, mockRes } from 'sinon-express-mock'
 import "reflect-metadata";
+let chai = require("chai");
+let sinon = require("sinon");
+var sinonChai = require("sinon-chai");
 
 import { UserController } from "./userController"
+import { User } from "../../../common/user/user"
+
+chai.should();
+chai.use(sinonChai);
 
 describe("Testing UserController methods", () => {
     let controller: UserController.Users;
+    let next: NextFunction
+    let user: User;
 
     beforeEach(() => {
         controller = new UserController.Users();
+        user = { name: "blah", regexpression: /a/ };
+    })
+
+    afterEach(() => {
+        sinon.restore()
     })
 
     describe("initialization", () => {
@@ -15,4 +31,14 @@ describe("Testing UserController methods", () => {
             assert.deepEqual(controller.users, [], "should be empty");
         });
     });
+
+    describe("getUsers", () => {
+        it("send an user list", () => {
+            controller.users.push(user)
+            let req = mockReq({ body: { name: 'blah' } });
+            let res = mockRes();
+            controller.get(req, res, next);
+            expect(res.send).to.have.been.calledWith(JSON.stringify(controller.users));
+        })
+    })
 });
