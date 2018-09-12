d479e2cf42ded17563ba4a6e8860777b73f465bc
jacob643 <jacobdorais25@hotmail.com>
2018-09-12 14:12:22 -0400

    updateview method

--- client/src/app/stopwatch/stopwatch.component.ts
+++ client/src/app/stopwatch/stopwatch.component.ts
@@ -20,6 +20,27 @@ export class StopwatchComponent implements OnInit {
     output: string;
 
 
+    updateview() {
+
+
+        // Time calculations minutes and seconds
+        let minutes = Math.floor(
+            this.elapsedTime / (MILI_IN_SEC * SEC_IN_MIN));
+        let seconds = Math.floor(
+            (this.elapsedTime % (MILI_IN_SEC * SEC_IN_MIN)) / MILI_IN_SEC);
+
+        if (minutes > 99) {
+            this.output = "99:59";
+        }
+        else {
+
+            let strsec = this.padding(seconds);
+            let strmin = this.padding(minutes);
+
+            this.output = strmin + ':' + strsec;
+        }
+    }
+
     padding(amount: number) {
         let str = amount + '';
         if (str.length < CHAR_IN_DISPLAY) str = '0' + str;
