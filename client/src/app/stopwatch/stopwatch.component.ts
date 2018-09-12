27c1a2b4eaf2beb9515acf1d53392cda02eed6f2
jacob643 <jacobdorais25@hotmail.com>
2018-09-12 14:04:43 -0400

    adding constants

--- client/src/app/stopwatch/stopwatch.component.ts
+++ client/src/app/stopwatch/stopwatch.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+const MILI_IN_SEC = 1000;
+const SEC_IN_MIN = 60;
+const CHAR_IN_DISPLAY = 2;
+
 @Component({
     selector: 'app-stopwatch',
     templateUrl: './stopwatch.component.html',
     styleUrls: ['./stopwatch.component.scss']
 })
+
 export class StopwatchComponent implements OnInit {
 
     startTime: number;
