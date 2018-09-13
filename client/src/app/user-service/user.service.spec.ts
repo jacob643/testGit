5074bb9e30da1afaee6e1f5cac3de7caaafca3d4
Billy <billy.bouchard03@gmail.com>
2018-09-13 12:46:22 -0400

    Add getUsers method and adjust test to make sure they work

--- client/src/app/user-service/user.service.spec.ts
+++ client/src/app/user-service/user.service.spec.ts
@@ -1,15 +1,32 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { User } from "../../../../common/user/user"
 import { UserService } from './user.service';
+import { TestHelper } from "../../test.helper";
+import { } from "jasmine";
+
+let httpClientSpy: any;
+let userService: UserService;
 
 describe('UserService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [UserService]
-    });
-  });
+    let user: User;
 
-  it('should be created', inject([UserService], (service: UserService) => {
-    expect(service).toBeTruthy();
-  }));
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
+        userService = new UserService(httpClientSpy);
+        user = { name: "Blah123", regexpression: /a/ };
+    });
+
+    describe("GetUsers", () => {
+        it("should get a list of user and only be called once", () => {
+            httpClientSpy.get.and.returnValue(TestHelper.asyncData([user]));
+            userService.getUsers().subscribe(
+                (response: User[]) => {
+                    expect(response.length).toEqual(1, "number check");
+                    expect(response[0]).toEqual(user, "check if its the good user");
+                },
+                fail
+            );
+            // check if only one call was made
+            expect(httpClientSpy.get.calls.count()).toBe(1, "one call");
+        })
+    })
 });
--- client/src/app/user-service/user.service.ts
+++ client/src/app/user-service/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
+
+import { User } from "../../../../common/user/user";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
 
-    constructor() { }
+    private readonly BASE_URL: string = "http://localhost:3000/users";
+    public constructor(private http: HttpClient) { }
+
+    public getUsers(): Observable<User[]> {
+        return this.http.get<User[]>(this.BASE_URL).pipe(
+            catchError(this.handleError<User[]>("GetUsers"))
+        );
+    }
+
+    public handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
+        return (error: Error): Observable<T> => {
+            return of(result as T);
+        };
+    }
 }
