4c641224428a08e4f0f9ad81159a438da650e757
jacob643 <jacobdorais25@hotmail.com>
2018-09-14 08:36:07 -0400

    test start and stop 5ms precision

--- client/src/app/stopwatch/stopwatch.component.spec.ts
+++ client/src/app/stopwatch/stopwatch.component.spec.ts
@@ -27,4 +27,21 @@ describe('StopwatchComponent', () => {
         assert(component.output == "00:00");
     });
 
+
+        it('should be greater than 0 after started', () => {
+
+            component.start();
+            let waitTime = 100;
+            let precision = 5;
+            let startTime = new Date().getTime();
+            let now = new Date().getTime();
+            while(now < startTime + waitTime)
+            {
+                now = new Date().getTime();
+            }
+            component.stop();
+            assert(waitTime - precision < component.elapsedTime
+                && component.elapsedTime < waitTime + precision);
+        });
+
 });
