55bb2fe5602f3aa0355501cbe0735cdbdeb4cd66
FX <francois-xavier.legault@polymtl.ca>
2018-09-14 12:28:57 -0400

    Added a function to create a scoreBoard with default parameters.

--- common/game/scoreBoard.ts
+++ common/game/scoreBoard.ts
@@ -1,8 +1,25 @@
-import { Score } from './score';
+import { Score, createScore } from './score';
 
 export interface ScoreBoard {
     id: Number;
     first: Score;
     second: Score;
     third: Score;
+}
+
+export function createScoreBoard(id = 0, first: Score = undefined, second: Score = undefined, third: Score = undefined): ScoreBoard {
+
+    if (!first) {
+        first = createScore();
+    }
+
+    if (!second) {
+        second = createScore();
+    }
+
+    if (!third) {
+        third = createScore();
+    }
+
+    return { id: id, first: first, second: second, third: third }
 }
