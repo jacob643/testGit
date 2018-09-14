d81971b9ff8d12aba62151fefc7b810d31449099
FX <francois-xavier.legault@polymtl.ca>
2018-09-14 12:23:54 -0400

    Added a function to create a game with default parameters.

--- common/game/game.ts
+++ common/game/game.ts
@@ -1,4 +1,4 @@
-import { ScoreBoard } from './scoreBoard';
+import { ScoreBoard, createScoreBoard } from './scoreBoard';
 
 export interface Game {
     id: Number;
@@ -7,4 +7,18 @@ export interface Game {
     score1v1: ScoreBoard;
     imagePath: string;
     singleView: boolean;
+}
+
+export function createGame(id = 0, name = "defaultName", scoreSolo: ScoreBoard = undefined,
+    score1v1: ScoreBoard = undefined, imagePath = "defaultPath", singleView = true): Game {
+
+    if (!scoreSolo) {
+        scoreSolo = createScoreBoard();
+    }
+
+    if (!score1v1) {
+        score1v1 = createScoreBoard();
+    }
+
+    return { id: id, name: name, scoreSolo: scoreSolo, score1v1: score1v1, imagePath: imagePath, singleView: singleView };
 }
