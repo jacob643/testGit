8aca7dadbb134c2b50f5be15c390e3d755d2637e
Audrey Germain <audrey.germain@polymtl.ca>
2018-09-15 15:55:46 -0400

    add post method in game service

--- client/src/app/services/game.service.ts
+++ client/src/app/services/game.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 import { Game, createGame } from './../../../../common/game/game';
-
+import { Observable } from "rxjs";
 @Injectable({
     providedIn: 'root'
 })
 export class GameService {
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
     generateExampleArray():Game[] {
         let gameExamples: Game[];
         gameExamples = [ createGame(), createGame(), createGame(), createGame(), createGame(), createGame()]
 
         return gameExamples;
+    }
+
+    public post(name: string): Observable<Game>{
+        return this.http.post<Game>("http://localhost:3000/games",{name});
     }
 }
