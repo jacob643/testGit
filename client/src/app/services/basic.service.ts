f7afa9a86a0c81a4b9964bf2bd9ab04e2be8b083
FX <francois-xavier.legault@polymtl.ca>
2018-09-16 04:30:16 -0400

    Prefixed unused variables with underscore.

--- client/src/app/services/basic.service.ts
+++ client/src/app/services/basic.service.ts
@@ -19,9 +19,9 @@ export class BasicService {
         );
     }
 
-    private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
+    private handleError<T>(_request: string, result?: T): (error: Error) => Observable<T> {
 
-        return (error: Error): Observable<T> => {
+        return (_error: Error): Observable<T> => {
             return of(result as T);
         };
     }
