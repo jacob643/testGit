8bab55d0d0684d868f3ff2d4ea545b2d0721c9b6
FX <francois-xavier.legault@polymtl.ca>
2018-09-16 06:57:21 -0400

    Component 'admin-view' created.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { InitialViewComponent } from './inital-view/initial-view.component';
 import { StopwatchComponent } from './stopwatch/stopwatch.component';
 import { AppRouterModule } from "./router/approuter.module";
 import { GameMenuComponent } from './game-menu/game-menu.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
 
 @NgModule({
     declarations: [
@@ -18,7 +19,8 @@ import { GameMenuComponent } from './game-menu/game-menu.component';
         InitialViewComponent,
         GameDisplayComponent,
         InitialViewComponent,
-        GameMenuComponent
+        GameMenuComponent,
+        AdminViewComponent
     ],
     imports: [
         BrowserModule,
--- client/src/app/router/approuter.module.ts
+++ client/src/app/router/approuter.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { InitialViewComponent } from '../inital-view/initial-view.component'
 import { GameMenuComponent } from '../game-menu/game-menu.component'
+import { AdminViewComponent } from '../admin-view/admin-view.component'
 
 export const routes: Routes = [
     { path: '', redirectTo: 'initial', pathMatch: 'full' },
     { path: 'initial', component: InitialViewComponent },
-    { path: 'gamelist', component: GameMenuComponent}
+    { path: 'gamelist', component: GameMenuComponent},
+    { path: 'admin', component: AdminViewComponent}
 ];
 
 @NgModule({
