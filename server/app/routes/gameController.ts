b775ed321ef52b88c3ac88dc459c12aba11fe0b8
Billy <billy.bouchard03@gmail.com>
2018-09-19 12:19:59 -0400

    Add constant to replace magic strings

--- server/app/routes/gameController.ts
+++ server/app/routes/gameController.ts
@@ -8,6 +8,8 @@ import { injectable, } from "inversify";
 export module GameController {
 
     const DB_URL: string = "mongodb://admin:npm2018*@ds157742.mlab.com:57742/npmdb";
+    const COLLECTION_NAME = "games"
+    const DB_NAME = "npmdb"
 
     @injectable()
     export class Games {
@@ -16,7 +18,7 @@ export module GameController {
                 if (err) {
                     throw err;
                 } else {
-                    var games = db.db("npmdb").collection("games");
+                    var games = db.db(DB_NAME).collection(COLLECTION_NAME);
                     games.find().toArray(function(_err: any, docs: any) {
                         res.send(JSON.stringify(docs));
                     });
@@ -32,7 +34,7 @@ export module GameController {
                     var scoreBoard: ScoreBoard = createScoreBoard();
                     var game: Game = createGame(1, req.body.name, scoreBoard, scoreBoard, "", true);
                     //post games
-                    db.db("npmdb").collection("games").insertOne(game, function(error: Error, _res: any) {
+                    db.db(DB_NAME).collection(COLLECTION_NAME).insertOne(game, function(error: Error, _res: any) {
                         if (error) throw error;
                         console.log("inserted game!");
                     });
@@ -45,7 +47,7 @@ export module GameController {
                 if (err) {
                     throw err;
                 } else {
-                    var games = db.db("npmdb").collection("games");
+                    var games = db.db(DB_NAME).collection(COLLECTION_NAME);
                     games.find({ singleView: true }).toArray(function(_err: Error, docs: any) {
                         res.send(JSON.stringify(docs));
                     });
@@ -58,7 +60,7 @@ export module GameController {
                 if (err) {
                     throw err
                 } else {
-                    var games = db.db("npmdb").collection("games");
+                    var games = db.db(DB_NAME).collection(COLLECTION_NAME);
                     games.find({ singleView: false }).toArray(function(_err: any, docs: any) {
                         res.send(JSON.stringify(docs));
                     });
