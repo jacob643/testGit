ac01cf046020f028f45c3639cae18cb7463eb3c0
FX <francois-xavier.legault@polymtl.ca>
2018-09-21 10:15:19 -0400

    start-game component now has a game attribute to show.

--- client/src/app/start-game/start-game.component.ts
+++ client/src/app/start-game/start-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from './../../../../common/game/game';
 
 @Component({
   selector: 'app-start-game',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-game.component.scss']
 })
 export class StartGameComponent implements OnInit {
+
+  game :Game;
 
   constructor() { }
 
