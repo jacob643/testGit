61fd67b982691e2333a595a22ce9a395a2616afd
Jared <jaredsnape@gmail.com>
2018-09-23 10:23:55 -0400

    Add imports to socket service

--- client/src/app/services/socket-service/socket.service.ts
+++ client/src/app/services/socket-service/socket.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { Message } from "../../../../../common/communication/message";
+import { Event } from "../../../../../common/communication/event";
+
+import * as socketIo from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
