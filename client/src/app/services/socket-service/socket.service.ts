d6fc5d3d54af0cf8a5a07ea42675c0aaa50fd4f3
Jared <jaredsnape@gmail.com>
2018-09-23 10:31:57 -0400

    Integrate socket.io in socket service

--- client/src/app/services/socket-service/socket.service.ts
+++ client/src/app/services/socket-service/socket.service.ts
@@ -5,10 +5,30 @@ import { Event } from "../../../../../common/communication/event";
 
 import * as socketIo from 'socket.io-client';
 
+const SERVER_URL = 'http://localhost:3000';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
 
-  constructor() { }
+    private socket: any;
+
+    public initSocket(): void {
+        this.socket = socketIo(SERVER_URL);
+    }
+
+    public onMessage(): Observable<Message> {
+        return new Observable<Message>(observer => {
+            this.socket.on('message', (data: Message) => observer.next(data));
+        });
+    }
+
+    public onEvent(event: Event): Observable<Event> {
+        return new Observable<Event>(observer => {
+            this.socket.on(event, () => observer.next());
+        });
+    }
+
+    constructor() { }
 }
