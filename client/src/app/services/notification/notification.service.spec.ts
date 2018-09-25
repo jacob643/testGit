1d02432ac0b11fd36ea5fd9dc48bd64757f7a4c7
Billy <billy.bouchard03@gmail.com>
2018-09-25 14:04:03 -0400

    Add test for notification service

--- client/src/app/services/notification/notification.service.spec.ts
+++ client/src/app/services/notification/notification.service.spec.ts
@@ -1,15 +1,24 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { NotificationService } from './notification.service';
+import { createNotification } from '../../../../../common/communication/notification';
 
 describe('NotificationService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [NotificationService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [NotificationService]
+        });
     });
-  });
 
-  it('should be created', inject([NotificationService], (service: NotificationService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([NotificationService], (service: NotificationService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    describe('clear', inject([NotificationService], (service: NotificationService) => {
+        it("should make the notification falsy", () => {
+            service.notification = createNotification();
+            service.clear();
+            expect(service.notification).toBeFalsy();
+        });
+    }));
 });
