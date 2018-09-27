cf502de7eadb1c31494d9a3341742ec2a945ea8c
FX <francois-xavier.legault@polymtl.ca>
2018-09-27 16:28:30 -0400

    Changed game-display component to use gameService instead of selected-game service.

--- client/src/app/game-display/game-display.component.ts
+++ client/src/app/game-display/game-display.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Game, createGame } from './../../../../common/game/game';
 import { Router } from '@angular/router'
-import { SelectedGameService } from '../services/selected-game.service'
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-game-display',
@@ -13,7 +13,7 @@ export class GameDisplayComponent implements OnInit {
   @Input() game :Game;
   @Input() admin: Boolean;
 
-  constructor( private router: Router, private selectGame: SelectedGameService) {
+  constructor( private router: Router, private gameService: GameService) {
       this.game = createGame();
   }
 
@@ -21,6 +21,6 @@ export class GameDisplayComponent implements OnInit {
 
   playGame() {
     this.router.navigate(['/game']);
-    this.selectGame.changeID(<number>this.game.id);
+    this.gameService.changeSelectedGameID(<number>this.game.id);
   }
 }
