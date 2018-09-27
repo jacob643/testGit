9e2ebcddfffa4b086a587bb638e65d2cd9774ea3
FX <francois-xavier.legault@polymtl.ca>
2018-09-27 16:07:32 -0400

    Imports and beforeEach for new tests in start-game component.

--- client/src/app/start-game/start-game.component.spec.ts
+++ client/src/app/start-game/start-game.component.spec.ts
@@ -1,29 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { StartGameComponent } from './start-game.component';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient, HttpHandler } from "@angular/common/http";
+import { Game, createGame } from "./../../../../common/game/game";
 import { } from "jasmine";
+import { GameService } from '../services/game.service';
+import { of } from '../../../node_modules/rxjs';
+import { HttpClientModule } from '../../../node_modules/@angular/common/http';
 
 describe('StartGameComponent', () => {
     let component: StartGameComponent;
     let fixture: ComponentFixture<StartGameComponent>;
+    let game: Game;
+    let gameService: GameService;
 
-    beforeEach(async(() => {
+    beforeEach( async(() => {
         TestBed.configureTestingModule({
             declarations: [StartGameComponent],
-            providers: [ HttpClient, HttpHandler, {provide : ActivatedRoute, useValue : routeStub }]
+            providers: [GameService],
+            imports: [HttpClientModule]
         })
             .compileComponents();
     }));
 
-    let routeStub: any;
-
-    beforeEach(() => {
+    beforeEach(inject([GameService], (g: GameService)=> {
+        game = createGame();
+        gameService = g;
+        spyOn(gameService, "getGameById").and.returnValue(of(game));
         fixture = TestBed.createComponent(StartGameComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-        routeStub = { queryParams: { id: null} };
-    });
+    }));
 
     it('should create', () => {
         expect(component).toBeTruthy();
